524d3157a9a671f5c88eb8cf1fe1f649bf4ae6e8
杜 世橋 Du Shiqiao <lucidfrontier.45@gmail.com>
2024-06-28 20:57:34 +0900

    implemented json write feature

--- src/pydanticio/json.py
+++ src/pydanticio/json.py
@@ -1,4 +1,7 @@
+from collections.abc import Iterable
 from typing import TextIO
+
+from pydantic import BaseModel, RootModel
 
 from .common import T
 
@@ -9,4 +12,15 @@ def read_record_from_reader(reader: TextIO, model: type[T]) -> T:
 
 
 def read_records_from_reader(reader: TextIO, model: type[T]) -> list[T]:
-    return [model.model_validate_json(line) for line in reader]
+    list_of_model = RootModel[list[model]]
+    return read_record_from_reader(reader, list_of_model).root
+
+
+def write_record_to_writer(writer: TextIO, record: BaseModel) -> None:
+    writer.write(record.model_dump_json())
+
+
+def write_records_to_writer(writer: TextIO, records: Iterable[T]) -> None:
+    list_of_model = RootModel[Iterable[T]]
+    record = list_of_model(root=records)
+    write_record_to_writer(writer, record)
--- tests/test_json.py
+++ tests/test_json.py
@@ -8,20 +8,47 @@ from . import TestClass, test_records
 
 def test_read_record_from_reader():
     record_str = test_records[0].model_dump_json()
-    rdr = StringIO(record_str)
-    record = json_io.read_record_from_reader(rdr, TestClass)
+    reader = StringIO(record_str)
+    record = json_io.read_record_from_reader(reader, TestClass)
     assert record == test_records[0]
 
 
 def test_read_records_from_reader():
-    records_str = "\n".join(record.model_dump_json() for record in test_records)
-    rdr = StringIO(records_str)
-    records = json_io.read_records_from_reader(rdr, TestClass)
+    records_str = ",".join(record.model_dump_json() for record in test_records)
+    records_str = f"[{records_str}]"
+    reader = StringIO(records_str)
+    records = json_io.read_records_from_reader(reader, TestClass)
     assert records == test_records
 
 
 def test_read_list_of_records_from_reader():
-    records_str = f"[{', '.join(record.model_dump_json() for record in test_records)}]"
-    rdr = StringIO(records_str)
-    record = json_io.read_record_from_reader(rdr, RootModel[list[TestClass]]).root
+    records_str = ",".join(record.model_dump_json() for record in test_records)
+    records_str = f"[{records_str}]"
+    reader = StringIO(records_str)
+    record = json_io.read_record_from_reader(reader, RootModel[list[TestClass]]).root
     assert record == test_records
+
+
+def test_write_record_to_writer():
+    record_str = test_records[0].model_dump_json()
+    writer = StringIO()
+    json_io.write_record_to_writer(writer, test_records[0])
+    assert writer.getvalue() == record_str
+
+
+def test_write_records_to_writer():
+    records_str = ",".join(record.model_dump_json() for record in test_records)
+    records_str = f"[{records_str}]"
+    writer = StringIO()
+    json_io.write_records_to_writer(writer, test_records)
+    assert writer.getvalue() == records_str
+
+
+def test_write_list_of_records_to_writer():
+    records_str = ",".join(record.model_dump_json() for record in test_records)
+    records_str = f"[{records_str}]"
+    writer = StringIO()
+    json_io.write_record_to_writer(
+        writer, RootModel[list[TestClass]](root=test_records)
+    )
+    assert writer.getvalue() == records_str
